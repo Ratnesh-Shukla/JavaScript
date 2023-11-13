a763aeb90d6a9062f6a8a257fd4d0bc22b52b04b
Ratnesh-Shukla <150715433+Ratnesh-Shukla@users.noreply.github.com>
2023-11-13 19:06:37 +0000

    variables

---
+++ JavaScript_Basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 465966
+let accountEmail = "sbc@google.com"
+var accountPassword = "457646"
+accountCity = "Java"    // we should not declare variables without their keywords
+let accountState
+// accountId = 54656     //changing value of contant variable is not allowed
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+accountEmail = "sdhv@gmail.com"
+accountPassword = "5454564"
+accountCity = "Script"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
