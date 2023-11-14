9b5a92e2a1baafc9e8eda529a9bad62c1ca03184
Ratnesh Shukla <150715433+Ratnesh-Shukla@users.noreply.github.com>
2023-11-14 18:23:28 +0000

    datat ypes

---
+++ JavaScript_Basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let name = "JavaScript"    // string data type it can be written inside single quotes also
+let age = 19    // it is number data type
+let isLoggedIn = true   // it is boolean
+let state     // it is under undefined data type
+
+console.log(typeof "JavaScript")   //string
+console.log(typeof age)    // number
+console.log(typeof isLoggedIn)          // boolean
+console.log(typeof undefined)      // type is undefined
+console.log(typeof null)          // its type is object
