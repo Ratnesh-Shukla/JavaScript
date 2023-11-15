b7f07d6a118c85f3342727a85b5548f6dc9a8f2d
Ratnesh Shukla <150715433+Ratnesh-Shukla@users.noreply.github.com>
2023-11-15 12:28:13 +0000

    operations

---
+++ JavaScript_Basics/opera.js
@@ -0,0 +1,32 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// string concatination
+let str1 = "Java"
+let str2 = "Script"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // here 1 is string so it prints 1; it treats 2 also as string and prints 2 (here complete operation is treated like string concatenation)
+// console.log( 1 + "2");  // here 1 is treated like a number but 2 is treated like a string
+// console.log("1" + 2 + 3); // here complete operation is treated as string concatenation 
+// console.log(1 + 2 + "3"); // first 1+2 operation is performed and we get 3 then 3 is printed as string
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let prefix = 50
+++prefix
+console.log(prefix);
+
+let postfix = 50
+postfix++
+console.log(postfix);
